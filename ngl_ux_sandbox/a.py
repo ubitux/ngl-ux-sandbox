dd58ead90e0321b6ba5e491d12a5989af109db89
Clément Bœsch <u@pkh.me>
2017-12-03 15:54:02 +0100

    wip

---
+++ ngl_ux_sandbox/a.py
@@ -0,0 +1,11 @@
+#from pynodegl_utils.misc import scene
+
+#@scene()
+#def test0(cfg):
+#    return None
+
+from b import x, y
+import b
+from b import z
+
+import d
---
+++ ngl_ux_sandbox/a2.py
@@ -0,0 +1,10 @@
+#from pynodegl_utils.misc import scene
+
+#@scene()
+#def test0(cfg):
+#    return None
+
+from b import x, y
+import b
+
+import d
